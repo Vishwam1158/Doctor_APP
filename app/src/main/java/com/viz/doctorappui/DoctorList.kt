92db455d1ad28e14135c21c44ac5453e3cfabc22
Vishwam1158 <vishwam1158@gmail.com>
2024-05-24 23:12:42 +0530

    add navigations

--- app/src/main/java/com/viz/doctorappui/DoctorList.kt
+++ app/src/main/java/com/viz/doctorappui/DoctorList.kt
@@ -1,7 +1,9 @@
 package com.viz.doctorappui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -13,10 +15,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
-//class DoctorList {
-//}
 
 @Composable
 fun DoctorList(navController: NavHostController) {
@@ -24,11 +25,13 @@ fun DoctorList(navController: NavHostController) {
         modifier = Modifier.fillMaxWidth()
     ) {
         Column(
-            modifier = Modifier.verticalScroll(rememberScrollState())
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(top = 52.dp, start = 4.dp)
         ) {
 
             Button(
-                onClick = { navController.navigate(Routes.contentScreen) },
+                onClick = { navController.navigate(Routes.homeScreen) },
                 colors = ButtonDefaults.buttonColors(Color.Transparent)
             ) {
                 Icon(
@@ -38,95 +41,102 @@ fun DoctorList(navController: NavHostController) {
                 )
             }
 
-            DoctorCard(
-                image = painterResource(id = R.drawable.doctor1),
-                doctorName = "dr. Gilang Segara Bening",
-                specilization = "Heart",
-                hospitalName = "Persahabatan Hospital",
-                rating = 1221,
-                status = "Open",
-                onClick = {
-                    DoctorDetails()
-                }
-            )
-            DoctorCard(
-                image = painterResource(id = R.drawable.doctor2),
-                doctorName = "dr. Shabil Chan",
-                specilization = "Dental",
-                hospitalName = "Columbia Asia Hospital",
-                rating = 964,
-                status = "Open",
-                onClick = {
-                    DoctorDetails()
-                }
-            )
-            DoctorCard(
-                image = painterResource(id = R.drawable.doctor3),
-                doctorName = "dr. Mustakim",
-                specilization = "Eye",
-                hospitalName = "Salemba Carolus Hospital",
-                rating = 762,
-                status = "Close",
-                onClick = {
-                    DoctorDetails()
-                }
-            )
-            DoctorCard(
-                image = painterResource(id = R.drawable.doctor4),
-                doctorName = "dr. Suprihatini",
-                specilization = "Heart",
-                hospitalName = "Salemba Carolus Hospital",
-                rating = 651,
-                status = "Open",
-                onClick = {
-                    DoctorDetails()
-                }
-            )
+            Spacer(modifier = Modifier.padding(top = 8.dp))
+           
+            TopDoctors(navController = navController)
 
             DoctorCard(
+                id = 5,
                 image = painterResource(id = R.drawable.doctor2),
                 doctorName = "dr. Maria Rodriguez",
-                specilization = "Dental",
-                hospitalName = "Persahabatan Hospital",
+                specilization = "Heart",
+                hospitalName = "Supernatants Hospital",
                 rating = 603,
                 status = "Open",
-                onClick = {
-                    DoctorDetails()
-                }
+                navController = navController
             )
             DoctorCard(
+                id = 6,
                 image = painterResource(id = R.drawable.doctor4),
                 doctorName = "dr. Aisha Patel",
                 specilization = "Eye",
                 hospitalName = "Columbia Asia Hospital",
                 rating = 571,
                 status = "Close",
-                onClick = {
-                    DoctorDetails()
-                }
+                navController = navController
             )
             DoctorCard(
+                id = 7,
                 image = painterResource(id = R.drawable.doctor1),
                 doctorName = "dr. John Smith",
                 specilization = "Heart",
                 hospitalName = "Salemba Carolus Hospital",
                 rating = 505,
                 status = "Open",
-                onClick = {
-                    DoctorDetails()
-                }
+                navController = navController
             )
             DoctorCard(
+                id = 8,
                 image = painterResource(id = R.drawable.doctor3),
                 doctorName = "dr. Michael Jones",
-                specilization = "Heart",
+                specilization = "Dental",
                 hospitalName = "Salemba Carolus Hospital",
                 rating = 473,
                 status = "Close",
-                onClick = {
-                    DoctorDetails()
-                }
+                navController = navController
             )
         }
+    }
+}
+
+
+@Composable
+fun TopDoctors(navController: NavHostController) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+
+
+
+        DoctorCard(
+            id = 1,
+            image = painterResource(id = R.drawable.doctor1),
+            doctorName = "dr. Gilang Segara Bening",
+            specilization = "Heart",
+            hospitalName = "Persahabatan Hospital",
+            rating = 1221,
+            status = "Open",
+            navController = navController
+        )
+        DoctorCard(
+            id = 2,
+            image = painterResource(id = R.drawable.doctor2),
+            doctorName = "dr. Shabil Chan",
+            specilization = "Dental",
+            hospitalName = "Columbia Asia Hospital",
+            rating = 964,
+            status = "Open",
+            navController = navController
+        )
+        DoctorCard(
+            id = 3,
+            image = painterResource(id = R.drawable.doctor3),
+            doctorName = "dr. Mustakim",
+            specilization = "Eye",
+            hospitalName = "Salemba Carolus Hospital",
+            rating = 762,
+            status = "Close",
+            navController = navController
+        )
+        DoctorCard(
+            id = 4,
+            image = painterResource(id = R.drawable.doctor4),
+            doctorName = "dr. Suprihatini",
+            specilization = "Heart",
+            hospitalName = "Salemba Carolus Hospital",
+            rating = 651,
+            status = "Open",
+            navController = navController
+        )
     }
 }
