9e233a3bb74355d8ecbbda70e6fa916c7a7540a6
Vishwam1158 <vishwam1158@gmail.com>
2024-05-24 23:10:55 +0530

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,11 @@ dependencies {
     // Navigation
     implementation(libs.androidx.navigation.compose)
 
+    implementation(libs.ui)
+    implementation(libs.androidx.material)
+    implementation(libs.ui.tooling.preview)
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
