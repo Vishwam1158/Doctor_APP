b3a21d4651d40c4c7445c6b4a3c370f006b2dcb3
Vishwam1158 <vishwam1158@gmail.com>
2024-05-24 23:12:05 +0530

    navigation works

--- app/src/main/java/com/viz/doctorappui/DoctorDetails.kt
+++ app/src/main/java/com/viz/doctorappui/DoctorDetails.kt
@@ -2,17 +2,55 @@ package com.viz.doctorappui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 
-@Preview
+
 @Composable
-fun DoctorDetails() {
+fun DoctorDetails(navController: NavHostController) {
     Column(
         modifier = Modifier.fillMaxWidth()
     ) {
-        Text(text = "Doctor List")
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(top = 52.dp, start = 4.dp)
+        ) {
+            Button(
+                onClick = {
+                    navController.popBackStack()
+                },
+                colors = ButtonDefaults.buttonColors(Color.Transparent)
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    tint = Color.Black,
+                    contentDescription = "Back"
+                )
+            }
+
+
+            Text(text = "Doctor Details")
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    DoctorDetails(navController = NavHostController(LocalContext.current))
 }
