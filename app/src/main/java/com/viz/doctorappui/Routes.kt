53b0826c8b3215613b4a8a644dccf178b6ff9feb
Vishwam1158 <vishwam1158@gmail.com>
2024-06-01 23:13:48 +0530

    add navigation through Screen with objects

--- app/src/main/java/com/viz/doctorappui/Routes.kt
+++ app/src/main/java/com/viz/doctorappui/Routes.kt
@@ -5,3 +5,18 @@ object Routes {
     var homeScreen =   "HomeScreen"
     var doctorList = "doctorList"
 }
+
+sealed class Screen(val route: String) {
+    object HomeScreen : Screen(Routes.homeScreen)
+    object DoctorList : Screen(Routes.doctorList)
+    object DoctorDetails : Screen(Routes.doctorDetails)
+
+    fun withArgs(vararg args: Int): String {
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/$arg")
+            }
+        }
+    }
+}
