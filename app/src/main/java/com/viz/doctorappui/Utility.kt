341d6502baaa60b816edd23008e624984b965bba
Vishwam1158 <vishwam1158@gmail.com>
2024-06-09 22:34:56 +0530

    update utility

--- app/src/main/java/com/viz/doctorappui/Utility.kt
+++ app/src/main/java/com/viz/doctorappui/Utility.kt
@@ -1,24 +1,38 @@
 package com.viz.doctorappui
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,12 +40,56 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import kotlin.random.Random
 
 // In this file we write codes for DoctorCard and ImageButton
+@Preview(showBackground = true)
+@Composable
+fun SearchBar() {
+    var text by remember { mutableStateOf("") }
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .padding(end = 16.dp),
+        shape = RoundedCornerShape(8.dp),
+        color = Color(0xFFF5F5F5)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            TextField(
+                value = text,
+                onValueChange = { text = it },
+                placeholder = { Text(text = "Search doctor, medicines etc", color = Color(0xFFC1C1C1)) },
+//                colors = TextFieldDefaults.textFieldColors(
+//                    backgroundColor = Color.Transparent,
+//                    focusedIndicatorColor = Color.Transparent,
+//                    unfocusedIndicatorColor = Color.Transparent,
+//                    cursorColor = Color(0xFF25282B)
+//                ),
+                modifier = Modifier.weight(1f)
+            )
+            Icon(
+                imageVector = Icons.Outlined.Search, // Ensure you have the search icon drawable
+                contentDescription = "Search Icon",
+                tint = Color(0xFF25282B),
+                modifier = Modifier.size(24.dp)
+            )
+        }
+    }
+}
+
+
 
 
 @Composable
@@ -56,7 +114,7 @@ fun CustomImageButton(backgroundColor : Color, icon : Painter, title : String, n
                 )
             }
             Spacer(modifier = Modifier.padding(4.dp))
-            Text(text = title, modifier = Modifier.align(Alignment.CenterHorizontally))
+            Text(text = title, modifier = Modifier.align(Alignment.CenterHorizontally), fontSize = 12.sp)
             Spacer(modifier = Modifier.padding(8.dp))
         }
 
@@ -66,7 +124,7 @@ fun CustomImageButton(backgroundColor : Color, icon : Painter, title : String, n
 
 
 
-@SuppressLint("InvalidColorHexValue")
+//@SuppressLint("InvalidColorHexValue")
 @Composable
 fun DoctorCard(
     id : Int,
@@ -179,21 +237,59 @@ fun DoctorCard(
 }
 
 
-//Appointment slot button
 @Composable
-fun SlotButton(time : String) {
+fun SlotButton(
+    time: String,
+    isSelected: Boolean,
+    onClick: () -> Unit,
+    enabled: Boolean
+) {
     Button(
-        onClick = { /*TODO*/ },
-        modifier = Modifier.padding(4.dp),
+        onClick = onClick,
+        modifier = Modifier
+            .padding(4.dp)
+            .size(120.dp, 60.dp),
         shape = RoundedCornerShape(10.dp),
-        colors = ButtonDefaults.buttonColors(Color(0xffffffff)),
-        border = ButtonDefaults.outlinedButtonBorder
+        colors = ButtonDefaults.buttonColors(
+            contentColor = Color.White,
+            containerColor = if (isSelected) Color(0xFF639AFF) else Color.White,
+            disabledContainerColor = Color.Gray
+        ),
+        enabled = enabled,
+        border = BorderStroke(1.dp, Color.Gray)
     ) {
-        Text(text = time, color = Color(0xFF404345), fontSize = 16.sp)
+        Text(
+            text = time,
+            color = if (enabled) Color(0xFF404345) else Color.LightGray,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
 
-//
+@Composable
+fun SlotButtonGroup(times: List<String>) {
+    var selectedButton by remember { mutableStateOf(-1) }
+
+    val randomStatus = times.map { Random.nextBoolean() }
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = Modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        itemsIndexed(times) { index, time ->
+            SlotButton(
+                time = time,
+                isSelected = index == selectedButton,
+                onClick = { selectedButton = index },
+                enabled = randomStatus[index]
+            )
+        }
+    }
+}
 //@Preview(showBackground = true)
 //@Composable
 //private fun Preview() {
@@ -214,35 +310,6 @@ fun SlotButton(time : String) {
 //        onClick = {
 //            DoctorDetails(navController)
 //        })
-//  
+//
 //    SlotButton(time = "10:00 AM")
 //}
-
-
-// CustomImageButton's optional code
-//@Preview
-//@Composable
-//private fun CustomButton() {
-//    Button(onClick = {},
-//        shape = RoundedCornerShape(10.dp),
-//        modifier = Modifier
-//            .size(64.dp, 64.dp)
-//            .clip(RoundedCornerShape(10.dp)) // must declare before the background color
-//            .height(64.dp)
-//            .width(64.dp)
-//
-//    ) {
-//            Icon(
-//                painter = painterResource(id = R.drawable.heart_03),
-//                modifier = Modifier
-//                    .height(32.dp)
-//                    .width(32.dp),
-//                contentDescription = "Image Button of ",
-//                tint = Color.White
-//            )
-//
-//    }
-//
-//}
-
-
