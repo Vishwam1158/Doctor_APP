7ea08106a030acdc2aa29482e36bbfc157eff474
Vishwam1158 <vishwam1158@gmail.com>
2024-06-09 22:46:05 +0530

    Split HomeScreen

--- app/src/main/java/com/viz/doctorappui/HomeScreen.kt
+++ app/src/main/java/com/viz/doctorappui/HomeScreen.kt
@@ -12,28 +12,26 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
-    Scaffold(
-        topBar = {
-            TopBar()
-        },
-        content = {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(it)
+
+    Column(
+modifier = Modifier.fillMaxSize()
             ) {
                 Column(
                     modifier = Modifier
@@ -41,7 +39,6 @@ fun HomeScreen(navController: NavHostController) {
                         .fillMaxSize()
                         .verticalScroll(rememberScrollState())
                 ) {
-
                     Row {
                         Text(
                             text = "Find ",
@@ -64,7 +61,7 @@ fun HomeScreen(navController: NavHostController) {
                             modifier = Modifier.fillMaxWidth(),
                             horizontalArrangement = Arrangement.SpaceAround
                         ) {
-                            CustomImageButton(backgroundColor = Color(0xFF639AFF), icon = painterResource(id = R.drawable.stethoscope_01), title = "Consult", navController = navController)
+                            CustomImageButton(backgroundColor = Color(0xFF639AFF), icon = painterResource(id = R.drawable.stethoscope_01), title = "Consultant", navController = navController)
                             CustomImageButton(backgroundColor = Color(0xFFA584FF), icon = painterResource(id = R.drawable.teeth_02), title = "Dental", navController = navController)
                             CustomImageButton(backgroundColor = Color(0xFFFF7854), icon = painterResource(id = R.drawable.heart_03), title = "Heart", navController = navController)
                             CustomImageButton(backgroundColor = Color(0xFFFEA725), icon = painterResource(id = R.drawable.clinic_04), title = "Hospitals", navController = navController)
@@ -74,7 +71,7 @@ fun HomeScreen(navController: NavHostController) {
                             modifier = Modifier.fillMaxWidth(),
                             horizontalArrangement = Arrangement.SpaceAround
                         ) {
-                            CustomImageButton(backgroundColor = Color(0xFF1AD37A), icon = painterResource(id = R.drawable.medicine_05), title = "Medicines", navController = navController)
+                            CustomImageButton(backgroundColor = Color(0xFF1AD37A), icon = painterResource(id = R.drawable.medicine_05), title = "Medicines ", navController = navController)
                             CustomImageButton(backgroundColor = Color(0xFF05D1ED), icon = painterResource(id = R.drawable.care_06), title = "Physician", navController = navController)
                             CustomImageButton(backgroundColor = Color(0xFFFF71C6), icon = painterResource(id = R.drawable.bandage_07), title = "Skin", navController = navController)
                             CustomImageButton(backgroundColor = Color(0xFFFF7070), icon = painterResource(id = R.drawable.syringe_08), title = "Surgeon", navController = navController)
@@ -93,7 +90,7 @@ fun HomeScreen(navController: NavHostController) {
                             Text(text = "Top Doctors", fontSize = 24.sp, color = Color(0XFF25282B))
                             Button(
                                 onClick = {
-                                    navController.navigate(Screen.DoctorList.route)
+                                    navController.navigate(Routes.DoctorList.route)
                                 },
                                 colors = ButtonDefaults.buttonColors( Color(0x00000000)),
                             ) {
@@ -103,20 +100,17 @@ fun HomeScreen(navController: NavHostController) {
                                     fontSize = 16.sp,
                                 )
                             }
-
-
-
                         }
                         Spacer(modifier = Modifier.padding(12.dp))
-
                         TopDoctors(navController = navController)
                     }
                 }
             }
-        }
-    )
 }
-
-
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen(navController = NavHostController(LocalContext.current))
+}
 
 //NavHostController(LocalContext.current)
